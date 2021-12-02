75e08ce3d4a2260df45263ada0ab41fb84c976e6
Killerwalski <pakowalski86@gmail.com>
2021-12-01 19:15:51 -0500

    Day1-1 Complete

--- Day1/depth.py
+++ Day1/depth.py
@@ -1,2 +1,14 @@
 depths = open("Day1\day1input.txt").readlines()
-print(depths[0])
+largerThanPrevious = 0
+current = int(depths[0])
+depths.pop(0)
+print("Starting point: " + str(current))
+for i in depths:
+    modifier = "(decreased)"
+    if int(i) > int(current):
+        largerThanPrevious += 1
+        modifier = "(increased)"
+    print(i + modifier)
+    current = i
+
+print(largerThanPrevious)
